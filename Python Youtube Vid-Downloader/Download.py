af3c38078a73f71ba9c5ccda808f6247e03e5053
Cevap05 <ademircevap1995@hotmail.com>
2022-08-29 23:47:31 +0000

    Changed py file

--- Python Youtube Vid-Downloader/Download.py
+++ Python Youtube Vid-Downloader/Download.py
@@ -3,6 +3,15 @@ from pytube import YouTube
 link = input("Paste YouTube link here: ")
 yt = YouTube(link)
 
+#Showing details.
+print("Downloading video: ",yt.title)
+print("Number of views: ",yt.views)
+print("Length of video: ",yt.length)
+print("Video rating: ",yt.rating)
+
+# Obtaining highest resolution possible.
 video = yt.streams.get_highest_resolution()
+
+print("Downloading...")
 video.download()
 print("Done!")
